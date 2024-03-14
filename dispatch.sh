d23d06cd3af89354dde06e66c863995090213315
Dennis Marttinen <twelho@welho.tech>
2024-03-14 18:21:03 +0200

    Update dispatch.sh

--- dispatch.sh
+++ dispatch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -l
 #SBATCH --job-name=container-evaluator
 #SBATCH --account=<project>
-#SBATCH --time=00:10:00
-#SBATCH --mem-per-cpu=2G
-#SBATCH --partition=small
+#SBATCH --partition=gputest
+#SBATCH --time=00:01:00
+##SBATCH --nodes=1
+##SBATCH --ntasks-per-node=1
+##SBATCH --cpus-per-task=1
+##SBATCH --mem-per-cpu=2G
+#SBATCH --output=output.txt
 
-srun ./evaluator.sh > "output-$(date +%F_%H-%M-%S).txt"
+srun ./evaluator.sh
+#srun ./evaluator.sh > "output-$(date +%F_%H-%M-%S).txt"
